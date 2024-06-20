9fbb08845d57e3e483ce1026e459060a1e890c1b
j-kleine <joschuadkleine@gmail.com>
2024-06-20 10:55:33 +0200

    finished Step 2 of L02

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
-const mongodb = require('./data/database');
 const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Contol-Allow-Headers',
+        'Origin, X-Requested-With, Content_Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
